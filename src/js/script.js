2391fc03d562daf757a1798452c5ed79fb466447
Eduardo Luis Borba <37090251+DuhBorba@users.noreply.github.com>
2020-07-25 00:25:47 -0300

    Refatoração do código

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,62 @@
-var redRange = document.querySelector('#range-red');
-var greenRange = document.querySelector('#range-green');
-var blueRange = document.querySelector('#range-blue');
+let redRange = document.querySelector('#range-red');
+let greenRange = document.querySelector('#range-green');
+let blueRange = document.querySelector('#range-blue');
 
-var redInput = document.querySelector('#input-red');
-var greenInput = document.querySelector('#input-green');
-var blueInput = document.querySelector('#input-blue');
+let redInput = document.querySelector('#input-red');
+let greenInput = document.querySelector('#input-green');
+let blueInput = document.querySelector('#input-blue');
 
-var borderColor = document.querySelector('.container');
+let borderColor = document.querySelector('.container');
 borderColor.style.borderColor = '#000';
 borderColor.style.boxShadow = '0px 0px 80px rgb(0,0,0)';
-var bgColor = document.querySelector('#bg-color');
+let bgColor = document.querySelector('#bg-color');
 bgColor.style.backgroundColor = '#000';
 
-var red = 0;
-var green = 0;
-var blue = 0;
+let red = 0;
+let green = 0;
+let blue = 0;
 
 function start() {
+  getRed();
+  getGreen();
+  getBlue();
+  renderColors();
+}
+
+function getRed() {
   redRange.addEventListener('input', function () {
-    var currentValueRed = redRange.value;
+    let currentValueRed = redRange.value;
     red = currentValueRed;
     redInput.value = currentValueRed;
-    renderColors(red);
+    renderColors();
   });
+}
 
+function getGreen() {
   greenRange.addEventListener('input', function () {
-    var currentValueGreen = greenRange.value;
+    let currentValueGreen = greenRange.value;
     green = currentValueGreen;
     greenInput.value = currentValueGreen;
-    renderColors(green);
+    renderColors();
   });
+}
 
+function getBlue() {
   blueRange.addEventListener('input', function () {
-    var currentValueBlue = blueRange.value;
+    let currentValueBlue = blueRange.value;
     blue = currentValueBlue;
     blueInput.value = currentValueBlue;
-    renderColors(blue);
+    renderColors();
   });
+}
 
-  function renderColors(r, g, b) {
-    r = red;
-    g = green;
-    b = blue;
-    bgColor.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    borderColor.style.borderColor = `rgb(${red}, ${green}, ${blue})`;
-    borderColor.style.boxShadow = `0px 0px 80px rgb(${red}, ${green}, ${blue})`;
-  }
+function renderColors() {
+  r = red;
+  g = green;
+  b = blue;
+  bgColor.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  borderColor.style.borderColor = `rgb(${red}, ${green}, ${blue})`;
+  borderColor.style.boxShadow = `0px 0px 80px rgb(${red}, ${green}, ${blue})`;
 }
 
 start();
